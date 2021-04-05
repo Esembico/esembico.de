0d74098984e2007a41ec788ca28aac65d6371ba3
sebabuck <46935066+sebabuck@users.noreply.github.com>
2021-04-05 18:52:13 +0200

    Tools Bugfix Calculation

--- tools/1_calculate_uic_number.js
+++ tools/1_calculate_uic_number.js
@@ -1,24 +1,23 @@
 function calculate()
 {
-     function calculate() {
-          const inputElement = $("#input-value");
-          const resultElement = $("#uic");
-          const arr = Array.from(String(inputElement.val()), Number);
-          arr.forEach(myFunction);
-
-          // number 31816650286 should be 6 1 16 1 12 6 10 0 4 8 12
-          function myFunction(item, index, arr) {
-              if (index % 2 === 0) {
-                  arr[index] = item * 2;
-              } else {
-                  arr[index] = item * 1;
-              }
-
-          }
-
-          const sum = arr.reduce(function (accumulator, current) {
-              return accumulator + (current < 10 ? current : current % 9);
-          });
-          resultElement.val(inputElement.val() + "" + sum % 10);
-          resultElement.trigger("change")
-      }
+	input_value = document.getElementById("input-value").value;
+	if (Number.isNaN(input_value)) return '';
+	var len = input_value.length;
+	var parity = len % 2;
+	var sum = 0;
+	for (var i = len-1; i >= 0; i--)
+	{
+		var d = parseInt(input_value.charAt(i));
+		if (i % 2 != parity) { d *= 2 }
+		if (d > 9) { d -= 9 }
+		sum += d;
+	}	 
+	check_number = 10 - (sum % 10).toString();
+	if (check_number == 10) {
+		check_number = 0;
+	}
+	document.getElementById("check_number").innerHTML = check_number
+		uic = input_value.concat(check_number);
+		document.getElementById("uic").innerHTML = uic
+return (sum % 10).toString();
+}
