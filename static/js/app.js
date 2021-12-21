c8fd6c9e5a3a4bbdba9fed7f0b433169ca2a1243
mbayer2303 <mbayer2303@gmail.com>
2021-12-21 19:05:56 +0100

    Added snow effect

--- static/js/app.js
+++ static/js/app.js
@@ -5,4 +5,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
       document.getElementById("main-menu").classList.toggle("active");
     });
   });
+
+  const today = new Date();
+  const year = today.getMonth() === 0 ? today.getFullYear() - 1 : today.getFullYear();
+  const firstDate = new Date(year, 11, 1)
+  const lastDate = new Date(year + 1, 0, 5)
+
+  if (today >= firstDate && today <= lastDate) {
+    const snow = document.createElement("div");
+    snow.className = "snow";
+    document.body.appendChild(snow);
+    const snow2 = document.createElement("div");
+    snow.className = "snow2";
+    document.body.appendChild(snow2);
+  }
 });
